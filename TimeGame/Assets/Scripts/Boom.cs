d3ef41a1b855a891a64a7aad9639bd511be306e4
Munch42 <munch4242@gmail.com>
2020-05-13 20:11:04 -0600

    Added Reverse Time Mechanic

---
+++ TimeGame/Assets/Scripts/Boom.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boom : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * Random.Range(1, 2), ForceMode2D.Impulse);
+            rb.AddTorque(Random.Range(1, 2), ForceMode2D.Impulse);
+        }
+    }
+}
